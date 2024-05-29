34eb58d2f84faba28bf1a557bd40eacf578d2887
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-29 14:17:49 -0400

    Create Redirect.php

---
+++ src/Resources/contao/library/RedirectManager/Model/Redirect.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Redirect Manager
+ *
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/redirect_manager
+ * @link       https://andrewstevens.consulting
+ */
+
+
+
+namespace RedirectManager\Model;
+
+use Contao\Model as Contao_Model;
+
+
+class Redirect extends Contao_Model
+{
+
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_asc_redirect';
+
+}
