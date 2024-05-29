cbf8fc043ae319042d95b4971fa02cc669e78c99
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-29 14:21:29 -0400

    Create BcsRedirectBundle.php

---
+++ src/BcsRedirectBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-popups
+ */
+
+namespace Bcs\RedirectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsRedirectBundle extends Bundle
+{
+}
