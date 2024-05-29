ba3b148ed9622852179c33443030c07d48d83868
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-29 12:31:24 -0400

    Create Redirect.php

---
+++ system/modules/contao_redirects/library/Bcs/Model/Redirect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bcs\Model;
+
+use Contao\Model as Contao_Model;
+
+
+class Redirect extends Contao_Model
+{
+
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_redirect';
+
+}
