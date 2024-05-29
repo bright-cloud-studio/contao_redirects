d66eb8831de5d70589127e81f968ea07747c3bb2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-29 12:24:15 -0400

    Create tl_redirect.php

---
+++ system/modules/contao_redirects/languages/en/tl_redirect.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_redirect']['category'] 			    = array('Category', 'Arbitrary text description. Searchable and filterable.');
+$GLOBALS['TL_LANG']['tl_redirect']['type'] 				      = array('Type', 'Type of Redirect');
+$GLOBALS['TL_LANG']['tl_redirect']['code'] 				      = array('Code', 'HTTP Status code. <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes" target="_blank">List of Status Codes</a>');
+$GLOBALS['TL_LANG']['tl_redirect']['redirect'] 			    = array('Redirect', 'Enter the old URL or patern to match.');
+$GLOBALS['TL_LANG']['tl_redirect']['redirect_domain'] 	= array('Redirect Domain', 'Enter the old domain.');
+$GLOBALS['TL_LANG']['tl_redirect']['target_domain'] 	  = array('Target Domain', 'Enter the new/target domain.');
+$GLOBALS['TL_LANG']['tl_redirect']['target_page'] 		  = array('Target Page', 'Please seleect the page to redirect to.');
+$GLOBALS['TL_LANG']['tl_redirect']['target_file'] 		  = array('Target File', 'Please select the file to redirect to.');
+$GLOBALS['TL_LANG']['tl_redirect']['target_url'] 		    = array('Target URL', 'Enter redirect target. Overrides Target Page attribute. Use $1 format for regex matches.');
+$GLOBALS['TL_LANG']['tl_redirect']['domain'] 			      = array('Domain', 'Limit this redirect to this domain only.');
+$GLOBALS['TL_LANG']['tl_redirect']['published'] 		    = array('Publish', 'Enable/Disable this redirect.');
+$GLOBALS['TL_LANG']['tl_redirect']['start'] 			      = array('Enable from', 'Enable this redirect on this date.');
+$GLOBALS['TL_LANG']['tl_redirect']['stop'] 				      = array('Disable after', 'Disable this redirect on this date.');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_redirect']['config_legend'] 	= 'Redirect details';
+$GLOBALS['TL_LANG']['tl_redirect']['redirect_legend'] 	= 'Redirect configuration';
+$GLOBALS['TL_LANG']['tl_redirect']['domain_legend'] 	= 'Domain restrictions';
+$GLOBALS['TL_LANG']['tl_redirect']['publish_legend'] 	= 'Publish';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_redirect']['new']    			= array('New redirect', 'Add a new redirect');
+$GLOBALS['TL_LANG']['tl_redirect']['show']   			= array('Redirect details', 'Show the details of redirect ID %s');
+$GLOBALS['TL_LANG']['tl_redirect']['edit']   			= array('Edit redirect', 'Edit redirect ID %s');
+$GLOBALS['TL_LANG']['tl_redirect']['copy']   			= array('Copy redirect', 'Copy redirect ID %s');
+$GLOBALS['TL_LANG']['tl_redirect']['delete'] 			= array('Delete redirect', 'Delete redirect ID %s');
+$GLOBALS['TL_LANG']['tl_redirect']['toggle'] 			= array('Toggle redirect', 'Toggle redirect ID %s');
