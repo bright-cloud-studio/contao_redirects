7ce39b814ed91e411fffaffc8a65ed64fea2ef15
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-29 14:16:03 -0400

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Redirect Manager
+ *
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/redirect_manager
+ * @link       https://andrewstevens.consulting
+ */
+
+
+
+/**
+ * Add a palette to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['redirect_404'] 	= '{title_legend},name,type';
