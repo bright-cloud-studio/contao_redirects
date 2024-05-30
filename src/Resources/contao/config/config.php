5df7cf4d142d1b0ce70e57beb9783d279a1f79b7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-30 08:19:03 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -11,22 +11,12 @@
 
 
 
-/**
- * Back end modules
- */
-if (!array_key_exists('redirect_manager', $GLOBALS['BE_MOD']))
-{
-    array_insert($GLOBALS['BE_MOD'], 1, array('redirect_manager' => array()));
-}
+//array_insert($GLOBALS['BE_MOD'], 1, array('redirect_manager' => array()));
 
-array_insert($GLOBALS['BE_MOD']['redirect_manager'], 0, array
-(
-	'redirects' => array
-	(
-		'tables' => array('tl_asc_redirect'),
-		'icon'   => '/system/modules/redirect_manager/assets/redirect.svg'
-	)
-));
+$GLOBALS['BE_MOD']['redirect_manager']['redirects'] = array(
+	'tables' => array('tl_asc_redirect'),
+	'icon'   => 'system/modules/oces_navigation/assets/icons/page_tag_navigation.png'
+);
 
 
 // Front end modules
