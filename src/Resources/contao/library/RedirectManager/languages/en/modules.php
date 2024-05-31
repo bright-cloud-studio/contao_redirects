d0731191416c85931db7409bf77668de28b2a978
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-31 09:09:57 -0400

    Create modules.php

---
+++ src/Resources/contao/library/RedirectManager/languages/en/modules.php
@@ -0,0 +1,9 @@
+<?php
+
+/** Front end modules */
+$GLOBALS['TL_LANG']['FMD']['redirect_manager'] 	= "Redirect Manager";
+$GLOBALS['TL_LANG']['FMD']['redirect_404']		= array('Redirect 404', 'Place this module on your 404 page to enable redirect manager.');
+
+/** Back end modules */
+$GLOBALS['TL_LANG']['MOD']['redirect_manager'] 	= array('Redirect Manager', 'Redirect Manager');
+$GLOBALS['TL_LANG']['MOD']['redirects'] 		= array('Redirects', 'Manage individual redirects');
