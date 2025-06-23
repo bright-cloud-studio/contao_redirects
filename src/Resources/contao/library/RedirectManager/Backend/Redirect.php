9fefbd6312671bcda6d812751e650ea89ec1e8df
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-23 09:45:17 -0400

    Update Redirect.php

--- src/Resources/contao/library/RedirectManager/Backend/Redirect.php
+++ src/Resources/contao/library/RedirectManager/Backend/Redirect.php
@@ -21,9 +21,13 @@ use Contao\Datacontainer;
 use Contao\FilesModel;
 use Contao\Image;
 use Contao\Input;
+use Contao\NewsModel;
 use Contao\PageModel;
 use Contao\StringUtil;
+use Contao\System;
 use Contao\Versions;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class Redirect extends Contao_Backend
@@ -35,28 +39,56 @@ class Redirect extends Contao_Backend
 		Database::getInstance()->prepare("UPDATE tl_asc_redirect SET published='' WHERE IF(stop = '', 0, CONVERT(stop, UNSIGNED)) < ? AND IF(stop = '', 0, CONVERT(stop, UNSIGNED)) > 0")->execute(time());
 	}
 
+
     public function generateLabel($row, $label, $dc, $args)
     {
-        $objRedirect = RedirectModel::findByPk($row['id']);
-
-		$strLabel = '<span class="category">[' .$objRedirect->category .']</span> <span class="code">' .$objRedirect->type .'</span>: <span class="redirect">' .$objRedirect->redirect ."</span>";
-
-		if ($objRedirect->target_url) {
-			$strLabel .= ' <span class="arrow">&rarr;</span> <span class="target">' .$objRedirect->target_url ."</span>";
- 		} else if ($objRedirect->target_page) {
-			$objPage = PageModel::findByPk($objRedirect->target_page);
-			if ($objPage) {
-				$strLabel .= ' <span class="arrow">&rarr;</span> <span class="page">' .$objPage->title ."</span>";
-			}
- 		} else if ($objRedirect->target_file) {
-			$objFile = FilesModel::findByUuid($objRedirect->target_file);
-			if ($objFile) {
-				$strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">' .$objFile->path ."</span>";
-			}
- 		}
-		$arg[0] = $strLabel;
-
-		return $strLabel;
+    	$objRedirect = RedirectModel::findByPk($row['id']);
+    
+        // If type is legacy, add tag to label to mark it, otherwise display like normal
+    	if($objRedirect->type == 'regular')
+    		$strLabel = '<span style="color:orange; font-weight:800;">[LEGACY]</span> <span class="category">[' .$objRedirect->category .']</span> <span class="code">' .$objRedirect->type .'</span>: <span class="redirect">' .$objRedirect->redirect ."</span>";
+    	else
+    		$strLabel = '<span class="category">[' .$objRedirect->category .']</span> <span class="code">regular</span>: <span class="redirect">' .$objRedirect->redirect ."</span>";
+        
+    	if ($objRedirect->target_url) {
+    		$strLabel .= ' <span class="arrow">&rarr;</span> <span class="target">' .$objRedirect->target_url ."</span>";
+    	} else if ($objRedirect->target_page) {
+    		$objPage = PageModel::findByPk($objRedirect->target_page);
+    		if ($objPage) {
+    			$strLabel .= ' <span class="arrow">&rarr;</span> <span class="page">' .$objPage->title ."</span>";
+    		}
+    	} else if ($objRedirect->target_file) {
+    		$objFile = FilesModel::findByUuid($objRedirect->target_file);
+    		if ($objFile) {
+    			$strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">' .$objFile->path ."</span>";
+    		}
+    	}  else if ($objRedirect->target) {
+    	    
+    	    // Using the new tag based system. Determine which tag we have to figure out what the URL is
+    	    
+    	    if(str_contains($objRedirect->target, 'link_url')) {
+    	        $page_id = $this->getPageIdFromInsertTag($objRedirect->target);
+    	        $objPage = PageModel::findOneBy(['id = ?'], [$page_id]);
+    	        if ($objPage) {
+					$strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">' . $objPage->getFrontendUrl() ."</span>";
+				} else {
+				    $strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">PAGE_NOT_FOUND</span>';
+				}
+    	    } else if(str_contains($objRedirect->target, 'news_url')) {
+    	        $news_id = $this->getNewsIdFromInsertTag($objRedirect->target);
+	            $objNews = NewsModel::findOneBy(['id = ?'], [$news_id]);
+				if ($objNews) {
+				    $newsUrl = System::getContainer()->get('contao.routing.content_url_generator')->generate($objNews, [], UrlGeneratorInterface::ABSOLUTE_PATH);
+					$strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">' .$newsUrl ."</span>";
+				} else {
+				    $strLabel .= ' <span class="arrow">&rarr;</span> <span class="file">NEWS_NOT_FOUND' .$objFile->path ."</span>";
+				}
+    	        
+    	    }
+    	}
+    	$arg[0] = $strLabel;
+    
+    	return $strLabel;
     }
 
 
@@ -108,5 +140,32 @@ class Redirect extends Contao_Backend
 
 		$objVersions->create();
 	}
+	
+	
+	//////////////////////
+	// HELPER FUNCTIONS //
+	//////////////////////
+	
+	public function getPageIdFromInsertTag($tag) {
+        
+        // Remove the first half of the tag
+        $cleaned = str_replace("{{link_url::","", $tag);
+        
+        // Remove the second half of the tag
+        $cleaned = str_replace("|urlattr}}","", $cleaned);
+
+        return $cleaned;
+    }
+    
+    public function getNewsIdFromInsertTag($tag) {
+        
+        // Remove the first half of the tag
+        $cleaned = str_replace("{{news_url::","", $tag);
+        
+        // Remove the second half of the tag
+        $cleaned = str_replace("|urlattr}}","", $cleaned);
+
+        return $cleaned;
+    }
 
 }
